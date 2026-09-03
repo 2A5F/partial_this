323a4d30e37f3e9b6b8024b2f6782481abdd7b06
U2A5F <U2A5F@outlook.com>
2026-09-03 21:27:23 +0800

    Improved documentation

--- partial_this/src/lib.rs
+++ partial_this/src/lib.rs
@@ -11,7 +11,6 @@
 //! use partial_this::partial;
 //!
 //! #[partial]
-//! #[derive(Debug)]
 //! pub struct Foo {
 //!     pub foo: i32,
 //!     pub bar: f32,
@@ -22,6 +21,7 @@
 //!         .with_foo(1)
 //!         .with_bar(1.0)
 //!         .done();
+//! 
 //!     assert_eq!(foo.foo, 1);
 //!     assert_eq!(foo.bar, 1.0);
 //! }
@@ -29,8 +29,8 @@
 //!
 //! # Multiple partial sources
 //!
-//! The initial storage can be a `Box`, an owned `MaybeUninit`, or a mutable
-//! reference, producing `Box<T>`, `T`, or `&mut T` respectively:
+//! The initial storage can be a `Box`, `Rc`, `Arc`, an owned `MaybeUninit`, or a mutable
+//! reference, producing `Box<T>`, `Rc<T>`, `Arc<T>`, `T`, or `&mut T` respectively:
 //!
 //! ```rust
 //! use partial_this::partial;
@@ -43,11 +43,25 @@
 //! }
 //!
 //! fn main() {
-//!     let a: Box<Foo> = Foo::partial(Box::new_uninit()).with_foo(1).with_bar(2.0).done();
-//!     let b: Foo = Foo::partial(MaybeUninit::uninit()).with_foo(1).with_bar(2.0).done();
+//!     // From Box<MaybeUninit<T>>
+//!     let a: Box<Foo> = Foo::partial(Box::new_uninit())
+//!         .with_foo(1)
+//!         .with_bar(2.0)
+//!         .done();
+//! 
+//!     // From MaybeUninit<T>
+//!     let b: Foo = Foo::partial(MaybeUninit::uninit())
+//!         .with_foo(1)
+//!         .with_bar(2.0)
+//!         .done();
+//! 
+//!     // From &mut MaybeUninit<T>
 //!     let mut buf = MaybeUninit::uninit();
-//!     let c: &mut Foo = Foo::partial(&mut buf).with_foo(1).with_bar(2.0).done();
-//!     let _ = (a, b, c);
+//!     let c: &mut Foo = Foo::partial(&mut buf)
+//!         .with_foo(1)
+//!         .with_bar(2.0).done();
+//! #
+//! #   let _ = (a, b, c);
 //! }
 //! ```
 //!
@@ -55,25 +69,59 @@
 //!
 //! Each field's builder method can be called in **any order**, but **at most
 //! once**:
-//!
+//! 
 //! ```rust
-//! use partial_this::partial;
-//!
+//! # use partial_this::partial;
+//! # 
 //! #[partial]
 //! pub struct Foo {
 //!     pub foo: i32,
 //!     pub bar: f32,
 //! }
+//! # fn main() {
+//! 
+//! let p = Foo::partial(Box::new_uninit());
+//! # _ = p;
+//! # }
+//! ```
+//! 
+//! ```compile_fail
+//! # use partial_this::partial;
+//! # 
+//! # #[partial]
+//! # pub struct Foo {
+//! #     pub foo: i32,
+//! #     pub bar: f32,
+//! # }
+//! #
+//! # fn main() {
+//! # let p = Foo::partial(Box::new_uninit());
+//! let p = p.with_foo(123);
+//! let p = p.with_foo(456);
+//! // Error: method cannot be called on `...` due to unsatisfied trait bounds
+//! # }
+//! ```
 //!
-//! fn main() {
-//!     // Fields can be set in any order.
-//!     let p = Foo::partial(Box::new_uninit());
-//!     let p = p.with_bar(1.0);
-//!     let p = p.with_foo(1); // `foo` was not set yet, so this is allowed
-//!     let foo = p.done();
-//!     assert_eq!(foo.foo, 1);
-//!     assert_eq!(foo.bar, 1.0);
-//! }
+//! ```rust
+//! # use partial_this::partial;
+//! # 
+//! # #[partial]
+//! # pub struct Foo {
+//! #     pub foo: i32,
+//! #     pub bar: f32,
+//! # }
+//! #
+//! # fn main() {
+//! # let p = Foo::partial(Box::new_uninit());
+//! #
+//! let p = p.with_bar(1.0);
+//! let p = p.with_foo(1); 
+//! // `foo` was not set yet, so this is allowed
+//!
+//! let foo = p.done();
+//! assert_eq!(foo.foo, 1);
+//! assert_eq!(foo.bar, 1.0);
+//! # }
 //! ```
 //!
 //! # Field access
@@ -81,52 +129,68 @@
 //! After a field is initialized you can read it or mutate it:
 //!
 //! ```rust
-//! use partial_this::partial;
-//!
+//! # use partial_this::partial;
+//! #
 //! #[partial]
 //! pub struct Foo {
 //!     pub foo: i32,
 //!     pub bar: f32,
 //! }
+//! 
+//! # fn main() {
+//! let mut p = Foo::partial(Box::new_uninit())
+//!     .with_foo(1);
 //!
-//! fn main() {
-//!     let mut p = Foo::partial(Box::new_uninit()).with_foo(1);
-//!     assert_eq!(*p.get_foo(), 1);
-//!     p.set_foo(123);
-//!     let foo = p.with_bar(2.0).done();
-//!     assert_eq!(foo.foo, 123);
-//! }
+//! assert_eq!(*p.get_foo(), 1);
+//!
+//! p.set_foo(123);
+//!
+//! let foo = p.with_bar(2.0)
+//!     .done();
+//!
+//! assert_eq!(foo.foo, 123);
+//! # }
 //! ```
 //!
 //! # Tuple and generic structs
 //!
-//! Tuple structs use `with_0`, `with_1`, ... as field method names; generic and
-//! lifetime-parameterized structs are also supported:
+//! Tuple structs use `0`, `1`, ... as field method names
 //!
 //! ```rust
-//! use partial_this::partial;
-//!
+//! # use partial_this::partial;
+//! #
 //! #[partial]
 //! pub struct Bar(i32, f32);
 //!
+//! # fn main() {
+//! let bar = Bar::partial(Box::new_uninit())
+//!     .with_0(1)
+//!     .with_1(2.0)
+//!     .done();
+//! 
+//! assert_eq!(bar.0, 1);
+//! assert_eq!(bar.1, 2.0);
+//! # }
+//! ```
+//! Generic and lifetime-parameterized structs are also supported:
+//! ```rust
+//! # use partial_this::partial;
+//! #
 //! #[partial]
 //! pub struct Pair<'a, T> {
 //!     pub name: &'a str,
 //!     pub value: T,
 //! }
+//! 
+//! # fn main() {
+//! let pair = Pair::<String>::partial(Box::new_uninit())
+//!     .with_name("x")
+//!     .with_value(String::from("y"))
+//!     .done();
 //!
-//! fn main() {
-//!     let bar = Bar::partial(Box::new_uninit()).with_0(1).with_1(2.0).done();
-//!     assert_eq!(bar.0, 1);
-//!     assert_eq!(bar.1, 2.0);
-//!
-//!     let pair = Pair::<String>::partial(Box::new_uninit())
-//!         .with_name("x")
-//!         .with_value(String::from("y"))
-//!         .done();
-//!     assert_eq!(pair.name, "x");
-//!     assert_eq!(pair.value, "y");
-//! }
+//! assert_eq!(pair.name, "x");
+//! assert_eq!(pair.value, "y");
+//! # }
 //! ```
 //!
 //! # Others
